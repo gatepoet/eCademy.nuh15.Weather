1e697dda5adee285706b35a324aef260f9bf1551
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2016-12-14 20:40:31 +0100

    Add project files.

---
+++ eCademy.nuh15.Weather/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace eCademy.nuh15.Weather
+{
+    [Activity(Label = "eCademy.nuh15.Weather", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
